37ed99c4e8880bfc7dbcc60df9ca84060ddfaf31
Nomad <yakushev_v@rambler.ru>
2026-03-18 15:02:39 +0300

    исправленное

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Vitali Yakushev");
-
-        int income = 44_000;    // доход
-        int spending = 30_000;  // расход
-
-        System.out.println("Итого (руб): ");
-        System.out.println(income - spending);
     }
 }
